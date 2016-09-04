fc428f3a59e91657b5c22c1bdfa0d19c00793346
MonalisaSamal16 <monalisa.samal12@gmail.com>
2016-09-04 14:40:44 +0530

    cachematrix.R
    Changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## I create makeCacheMatrix as a list containing a function for the following activities
+## 1 First set the value of a matrix
+## 2 Second get the value of the matrix
+## 3 Alternatively setting next the value of inverse of the matrix
+## 4 and last is get the value of inverse of the matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makecachematrix<-function(x = matrix()) {
+  inverse1 <- NULL
+  set <- function(n) {
+    x <<- n
+    inverse1 <<- NULL
+  }
+  get <- function() x
+  
+  setInversefun <- function(inverse) inverse1 <<- inverse
+  getInversefun <- function()inverse1
+  list( set = set, get = get, setInversefun=setInversefun, getInversefun=getInversefun)
 }
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache.
 
 
-## Write a short comment describing this function
+cachesolve <- function(x, ...){
+  inverse1 <- x$getInversefun()
+  if (!is.null(inverse1)){
+    message("get cache data")
+    return(inverse1)
+  }
+  matrixnew <- x$get()
+  inverse1 <- solve(matrixnew, ...)
+  x$setInversefun(inverse1)
+  inverse1
+  
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+source('C:/Users/monalisa.samal/Desktop/IIMB/Module 3/Day 5/Matrix.R')
+
+##test the matrix
+testmatrix<- makecachematrix(matrix( 1:6, 2, 2))
+testmatrix$get()
+testmatrix$getInversefun()
+cachesolve(testmatrix)
+
+testmatrix$set(matrix(c(10,20,30,40),2,2))
+testmatrix$get()
+testmatrix$getInversefun()
+cachesolve(testmatrix)
